b303d1c08e91b446ebf9ebd87d3c29e6958d5716
= <=>
2024-04-13 15:49:08 -0600

    Se maqueta el layout y la tabla de la pagina index

--- app.vue
+++ app.vue
@@ -1,12 +1,23 @@
-<!-- <template>
-  <div>
-    <NuxtWelcome />
-  </div>
-</template> -->
+
 <template>
   <NuxtLayout>
     <v-app>
-      <NuxtPage />
+      <template>
+        <v-layout class="rounded rounded-md">
+          <v-app-bar title="Application bar"></v-app-bar>
+
+          <v-navigation-drawer>
+            <v-list>
+              <v-list-item title="Navigation drawer"></v-list-item>
+            </v-list>
+          </v-navigation-drawer>
+
+          <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+            <NuxtPage />
+          </v-main>
+        </v-layout>
+      </template>
+      
     </v-app>
   </NuxtLayout>
 </template>
