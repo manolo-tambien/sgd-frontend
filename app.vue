d75480b5586fb0c8708c43f88d4eb9910dd0ed60
= <=>
2024-04-12 18:40:44 -0600

    Se instala Vuetify

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
-<template>
+<!-- <template>
   <div>
     <NuxtWelcome />
   </div>
+</template> -->
+<template>
+  <NuxtLayout>
+    <v-app>
+      <NuxtPage />
+    </v-app>
+  </NuxtLayout>
 </template>
